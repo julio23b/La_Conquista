ff1ebd01177280afd4ea2f8fe46839533d2d6d09
julio <114879531+julio23b@users.noreply.github.com>
2025-01-28 07:49:39 -0300

    Update index.js

--- asset/js/index.js
+++ asset/js/index.js
@@ -3,24 +3,25 @@ const productos = [
     { tipo: "Alfombras", medida: "50cm", img: "asset/media/Productos/Alfomabra/alfom2.webp", descripción: "Alfombra tipo Handira redonda", },
     { tipo: "Alfombras", medida: "70cm", img: "asset/media/Productos/Alfomabra/alfom3.webp", descripción: "Alfombra tipo Handira redonda", },
     { tipo: "Alfombras", medida: "60x40cm", img: "asset/media/Productos/Alfomabra/alfom4.webp", descripción: "Alfombra tipo Handira ovalada", },
-    { tipo: "Alfombras", medida: "60x40cm", img: "asset/media/Productos/Alfomabra/alfom5.webp", descripción: "Alfombra tipo Handira ovalada", },
-    { tipo: "Alfombras", medida: "50x50cm", img: "asset/media/Productos/Alfomabra/alfom6.webp", descripción: "Alfombra tipo Handira corazón", },
+    { tipo: "Alfombras", medida: "60x80cm", img: "asset/media/Productos/Alfomabra/alfom5.webp", descripción: "Alfombra tipo Handira rectangular", },
     { tipo: "Alfombras", medida: "70x90cm", img: "asset/media/Productos/Alfomabra/alfom7.webp", descripción: "Alfombra tipo Handira corazón", },
-    { tipo: "Alfombras", medida: "60x60cm", img: "asset/media/Productos/Alfomabra/alfom9.webp", descripción: "Alfombra tipo Handira rectangular", },
-    { tipo: "Alfombras", medida: "60x40cm", img: "asset/media/Productos/Alfomabra/alfom10.webp", descripción: "Alfombra tipo Handira rectangular", },
-    { tipo: "Alfombras", medida: "50x90cm", img: "asset/media/Productos/Alfomabra/alfom11.webp", descripción: "Alfombra tipo Handira corazón", },
+    { tipo: "Alfombras", medida: "60x120cm", img: "asset/media/Productos/Alfomabra/alfom10.webp", descripción: "Alfombra tipo Handira rectangular", },
+    { tipo: "Alfombras", medida: "70x90cm", img: "asset/media/Productos/Alfomabra/alfom11.webp", descripción: "Alfombra tipo Handira corazón", },
     { tipo: "Alfombras", medida: "60x80cm", img: "asset/media/Productos/Alfomabra/alfom12.webp", descripción: "Alfombra tipo Handira rectangular", },
     { tipo: "Alfombras", medida: "50x50cm", img: "asset/media/Productos/Alfomabra/alfom13.webp", descripción: "Alfombra tipo Handira corazón", },
     { tipo: "Alfombras", medida: "60x80cm", img: "asset/media/Productos/Alfomabra/alfom14.webp", descripción: "Alfombra tipo Handira rectagular", },
     { tipo: "Alfombras", medida: "70x90cm", img: "asset/media/Productos/Alfomabra/alfom15.webp", descripción: "Alfombra tipo Handira corazón blanco", },
-    { tipo: "Alfombras", medida: "60x40cm", img: "asset/media/Productos/Alfomabra/alfom16.webp", descripción: "Alfombra tipo Handira rectangular", },
-    { tipo: "Alfombras", medida: "60x40cm", img: "asset/media/Productos/Alfomabra/alfom17.webp", descripción: "Alfombra tipo Handira corazón", },
+    { tipo: "Alfombras", medida: "60x120cm", img: "asset/media/Productos/Alfomabra/alfom16.webp", descripción: "Alfombra tipo Handira rectangular", },
+    { tipo: "Alfombras", medida: "50x50cm", img: "asset/media/Productos/Alfomabra/alfom17.webp", descripción: "Alfombra tipo Handira corazón", },
+    { tipo: "Alfombras", medida: "50x50cm", img: "asset/media/Productos/Alfomabra/veteada.webp", descripción: "Alfombra veteada", },
+    { tipo: "Alfombras", medida: "50x50cm", img: "asset/media/Productos/Alfomabra/veteada1.webp", descripción: "Alfombra veteada", },
+    { tipo: "Alfombras", medida: "50x50cm", img: "asset/media/Productos/Alfomabra/veteada2.webp", descripción: "Alfombra veteada", },
     { tipo: "Caminos de Mesa", medida: "35x160cm", img: "asset/media/Productos/Camino de mesa/cmesa1.webp", descripción: "Camino de mesa blanco", },
     { tipo: "Caminos de Mesa", medida: "35x160cm", img: "asset/media/Productos/Camino de mesa/cmesa2.webp", descripción: "Camino de mesa blanco y marrón", },
     { tipo: "Caminos de Mesa", medida: "35x160cm", img: "asset/media/Productos/Camino de mesa/cmesa4.webp", descripción: "Camino de mesa gris", },
     { tipo: "Caminos de Mesa", medida: "35x160cm", img: "asset/media/Productos/Camino de mesa/cmesa5.webp", descripción: "Camino de mesa blanco con marrón", },
     { tipo: "Elementos de Limpieza", medida: "35x40cm", img: "asset/media/Productos/Elementos de limpieza/rejilla1.webp", descripción: "Rejilla gris", },
-    { tipo: "Elementos de Limpieza", medida: "35x40cm", img: "asset/media/Productos/Elementos de limpieza/rejilla2.webp", descripción: "Rejilla pesada blanca", },
+    { tipo: "Elementos de Limpieza", medida: "35x45cm", img: "asset/media/Productos/Elementos de limpieza/rejilla2.webp", descripción: "Rejilla pesada blanca", },
     { tipo: "Elementos de Limpieza", medida: "50x50cm", img: "asset/media/Productos/Elementos de limpieza/tpiso1.webp", descripción: "Trapo de piso gris", },
     { tipo: "Elementos de Limpieza", medida: "50x50cm", img: "asset/media/Productos/Elementos de limpieza/tpiso2.webp", descripción: "Trapo de piso blanco", },
 ];
@@ -43,7 +44,7 @@ const generarSubProductos = (productos) => {
                 <div class="card-back">
                     <img src="${producto.img || defaultImg}" alt="${producto.tipo}" loading="lazy">
                     <h2>${producto.descripción}</h2>
-                    <p>Medidas: ${producto.medida}</p>
+                    <p><strong>Medidas: ${producto.medida}</strong></p>
                 </div>
             </div>
         </div>
